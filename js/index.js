e1b07221f354299750db9a252dda67d8e82d9b7f
cmdeveloped <cmwebdevs@gmail.com>
2017-06-08 17:31:08 -0500

    Added scroll to all learn more buttons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-  $('.learnMore').click(function() {
+
+  // Scroll to Info Section
+  $('#scrollOne.learnMore').click(function() {
     $('html, body').animate({
     scrollTop: $('#info').offset().top
     }, 2000);
-  })
-})
+  });
+
+  $('#scrollTwo.learnMore').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#concept').offset().top
+    }, 2000);
+  });
+
+  $('#scrollThree.learnMore').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#create').offset().top
+    }, 2000);
+  });
+
+  $('#scrollFour.learnMore').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#confidence').offset().top
+    }, 2000);
+  });
+});
