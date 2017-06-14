8fdf365933e2414d1d3a768889d14c2a515faaa1
cmdeveloped <cmwebdevs@gmail.com>
2017-06-14 02:15:13 -0500

    Add auto-scroll to top and fadein of prompt message after user scrolls to bottom. Delay is 6 seconds.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,48 @@
 $(document).ready(function() {
+  var $doc = $('html, body');
+  var $scroll1 = $('#scrollOne.learnMore');
+  var $scroll2 = $('#scrollTwo.learnMore');
+  var $scroll3 = $('#scrollThree.learnMore');
+  var $scroll4 = $('#scrollFour.learnMore');
+  var $findID = $('#findOut');
+  var $findClass = $('.findOut');
 
   // Scroll to Info Section
-  $('#scrollOne.learnMore').click(function() {
-    $('html, body').animate({
+  $scroll1.click(function() {
+    $doc.animate({
     scrollTop: $('#info').offset().top
     }, 2000);
   });
 
-  $('#scrollTwo.learnMore').click(function() {
-    $('html, body').animate({
+  $scroll2.click(function() {
+    $doc.animate({
       scrollTop: $('#concept').offset().top
     }, 2000);
   });
 
-  $('#scrollThree.learnMore').click(function() {
-    $('html, body').animate({
+  $scroll3.click(function() {
+    $doc.animate({
       scrollTop: $('#create').offset().top
     }, 2000);
   });
 
-  $('#scrollFour.learnMore').click(function() {
-    $('html, body').animate({
+  $scroll4.click(function() {
+    $doc.animate({
       scrollTop: $('#confidence').offset().top
     }, 2000);
   });
 
-  $('#findOut').click(function() {
-    $('.findOut').slideDown();
-    $('#findOut').fadeOut();
+  $findID.click(function() {
+    $findClass.slideDown();
+    $findID.fadeOut();
   });
+
+  $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       $('body').delay(6000).animate({scrollTop: 0}, 2000, easing='swing');
+       $('.goodFit').delay(6000).fadeIn(4000);
+
+   }
+  });
+
 });
